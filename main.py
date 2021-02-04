b573eb6e0eb463f86e411f18c9b5a7d7af0b65b2
kevin g.f.simas <pacogamer55@gmail.com>
2021-02-04 17:47:57 -0300

    CommitDenovo

--- main.py
+++ main.py
@@ -3,41 +3,37 @@ import os
 from colorama import Style,Fore,Back,init
 from modules import get_mac, scan
 
-try:
-    init()
-    ip = input("Digite o ip:")
-    os.system("cls")
+init()
+ip = input("Digite o ip:")
+os.system("cls")
+analise = scan.AnaliseRede(ip)
+thread = analise.Start()
 
-    analise = scan.AnaliseRede(ip)
-    thread = analise.Start()
+print(6*" "+"      IPs             |        STATUS          ")
+print(6*" "+"                      |                        ")
 
-    print(6*" "+"      IPs             |        STATUS          ")
-    print(6*" "+"                      |                        ")
+for text in analise.lista:
+    print(text)
 
-    for text in analise.lista:
-        print(text)
+content = subprocess.check_output("arp -a")
+mac = get_mac.Mac(content)
+mac.GetMac()
+lista = mac.macs
+device = scan.Device()
+print(Style.BRIGHT+Fore.WHITE+"\nDispositivos[...]\n")
 
-    content = subprocess.check_output("arp -a")
-    mac = get_mac.Mac(content)
-    mac.GetMac()
-    lista = mac.macs
-    device = scan.Device()
-    print(Style.BRIGHT+Fore.WHITE+"\nDispositivos[...]\n")
+for i in range(0,len(lista)):
+    ip = mac.ips[i]
+    device.GetDevice(mac.macs[i],ip)
 
-    for i in range(0,len(lista)):
-        ip = mac.ips[i]
-        device.GetDevice(mac.macs[i],ip)
+for text in device.lista_msg:
+    try:
+        content = text.replace("Company","")
+        print(content)
+    except:
+        content = text
+        print(content)
 
-    for text in device.lista_msg:
-        try:
-            content = text.replace("Company","")
-            print(content)
-        except:
-            content = text
-            print(content)
-
-        
-    while True:
-        continue
-except Exception as error:
-    print(error)
+    
+while True:
+    continue
