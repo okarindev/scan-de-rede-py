80c0d3abd876556fa5db6d45cc8bc40671309ade
kevin g.f.simas <pacogamer55@gmail.com>
2021-02-04 17:50:47 -0300

    CommitArrumando

--- main.py
+++ main.py
@@ -3,17 +3,21 @@ import os
 from colorama import Style,Fore,Back,init
 from modules import get_mac, scan
 
+
 init()
 ip = input("Digite o ip:")
 os.system("cls")
 analise = scan.AnaliseRede(ip)
 thread = analise.Start()
 
+
 print(6*" "+"      IPs             |        STATUS          ")
 print(6*" "+"                      |                        ")
 
+
 for text in analise.lista:
     print(text)
+
 
 content = subprocess.check_output("arp -a")
 mac = get_mac.Mac(content)
@@ -22,9 +26,11 @@ lista = mac.macs
 device = scan.Device()
 print(Style.BRIGHT+Fore.WHITE+"\nDispositivos[...]\n")
 
+
 for i in range(0,len(lista)):
     ip = mac.ips[i]
     device.GetDevice(mac.macs[i],ip)
+
 
 for text in device.lista_msg:
     try:
@@ -34,6 +40,6 @@ for text in device.lista_msg:
         content = text
         print(content)
 
-    
+
 while True:
     continue
